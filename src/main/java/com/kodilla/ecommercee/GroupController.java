7a1ab71e94035986e59eb954b2da110c50a2023d
Pawel Pyrzyk <ppyrzyk@gmail.com>
2023-03-07 22:04:53 +0100

    GroupController endpoints created and GroupDto class

--- src/main/java/com/kodilla/ecommercee/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/GroupController.java
@@ -1,4 +1,36 @@
 package com.kodilla.ecommercee;
 
+import domain.GroupDto;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/group")
+@CrossOrigin
+
 public class GroupController {
+
+    @GetMapping
+    public List<GroupDto> getGroups () {
+        List<GroupDto> groupsList = new ArrayList<>();
+        return groupsList;
+    }
+    @GetMapping(value = "{groupId}")
+    public GroupDto getGroupById( @PathVariable long groupId ) {
+        return new GroupDto();
+    }
+
+    @PostMapping
+    public void createGroup(@RequestBody GroupDto groupDto) {
+
+    }
+    @PutMapping(value ="{groupId}")
+    public GroupDto updateGroupById( @PathVariable long groupId) {
+        return new GroupDto();
+    }
+
 }
