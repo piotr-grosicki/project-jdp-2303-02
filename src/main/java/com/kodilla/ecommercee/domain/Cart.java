c9ca00def88a6c7740ee1314cb63ddef0a2aabe0
Artur Stopyra <astopyra@gmail.com>
2023-03-10 22:12:46 +0100

    Create entity Cart and interface CartRepository

---
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -0,0 +1,28 @@
+package com.kodilla.ecommercee.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID")
+    private User user;
+
+    @ManyToMany(cascade = CascadeType.ALL, mappedBy = "carts")
+    private List<Product> products = new ArrayList<>();
+
+}
